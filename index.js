64a46555ecf4831ef9c39597bc9a1328dec5c1ea
4nth0ny1 <acatullo4@gmail.com>
2021-12-21 19:24:56 -0600

    add post route

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-const express = require("express");
+const express = require('express');
 const mysql = require('mysql2');
+const cors = require("cors");
 
+// add cors 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-// const db = mysql.createConnection({
-//     user: "root", 
-//     host: 'localhost', 
-//     password: "@f133tAdmin", 
-//     database: "bugTrackerLogin",
-// });
+const db = mysql.createConnection({
+    user: "root",
+    host: "127.0.0.1",
+    password: "@f133tAdmin",
+    database: "login",
+});
+
+app.post('/register', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query("INSERT INTO users (username, password) VALUES (?,?)",
+        [
+            username, 
+            password
+        ], 
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send('values inserted');
+            }
+        }
+    );
+});
 
 app.listen(3001, () => {
     console.log('running server');
---
+++ readme.md
@@ -0,0 +1,1 @@
+https://github.com/4nth0ny1/project-bug-tracker-client
