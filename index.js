971774e1060daffa726d9cf7d64af53c2a3f3a68
4nth0ny1 <acatullo4@gmail.com>
2021-12-21 23:04:10 -0600

    add login to backend

--- index.js
+++ index.js
@@ -35,6 +35,29 @@ app.post('/register', (req, res) => {
     );
 });
 
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query("SELECT * FROM users WHERE username = ? AND password = ?",
+        [
+            username, 
+            password
+        ], 
+        (err, result) => {
+            if (err) {
+                res.send({err: 'error bro'})
+            } else {
+                if (result.length > 0) {
+                    res.send(result);
+                } else {
+                    res.send({message: "invalid username or password"});
+                }
+            }
+        }
+    );
+});
+
 app.listen(3001, () => {
     console.log('running server');
 })
